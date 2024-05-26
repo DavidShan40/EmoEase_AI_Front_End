aa581d304f93f91cd9ed905d5ca4cfbe2c6dd7a2
TAO (DAVID) SHAN <shan2000814@gmail.com>
2024-05-26 11:45:45 -0400

    1

---
+++ src/components/Home/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useHistory } from 'react-router-dom'; // 导入 useHistory
+import homeLogo from "../../Assets/home-main.svg";
+import Home2 from "./Home2";
+
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+              Unlock Your Dream School with AI: Simplify Your Application Process Today{" "}
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Button className="demo-button" >Try Our Demo</Button>
+          
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
