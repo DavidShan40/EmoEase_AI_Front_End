6579595b5f6d5c3f3b10447496018fa2f7dad198
noonkho <noonkho@gmail.com>
2024-06-02 00:08:28 -0400

    Update: Try Demo button size adjust

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate, Link } from "react-router-dom";
 import homeLogo from "../../Assets/EmoEase_AI_Main_Page_Image.png";
 import Home2 from "./Home2";
 
 function Home() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   const handleDemoClick = () => {
-    navigate("/demo");
+    // navigate("/demo");
+    window.location.href = 'https://emoeaseai.streamlit.app';
   };
 
   return (
@@ -16,16 +17,31 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Container className="home-content">
           <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Find Your Inner Peace with AI: Effortlessly Relieve Stress and Boost Your Mood Today
-              </h1>
+            <Col md={7}>
+              <div className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Find Your Inner Peace with AI: Effortlessly Relieve Stress and Boost Your Mood Today
+                </h1>
+              </div>
+              
 
-              <div style={{ padding: 50, textAlign: "left" }}>
+              {/* <div style={{ height: '10rem', width: '16rem', textAlign: "left" }}> */}
+              {/* <div>
                 <Button className="demo-button" onClick={handleDemoClick}>
                   Try Our Demo
                 </Button>
+              </div> */}
+              <div onClick={handleDemoClick}>
+                <a
+                    href="https://emoeaseai.streamlit.app"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="demo-button"
+                >
+                  Try Out Demo
+                </a>
               </div>
+              
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
